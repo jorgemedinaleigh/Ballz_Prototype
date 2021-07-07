9e0f7af6ad7aae424324882c1326cfa42b8aa79e
Jorge Medina <jorgemedinaleigh@protonmail.com>
2021-07-06 23:06:23 -0400

    Added an enemy that spawns at a random position an follows the player

--- Assets/Scenes/Scripts/RotateCamera.cs
+++ Assets/Scenes/Scripts/RotateCamera.cs
@@ -17,5 +17,5 @@ public class RotateCamera : MonoBehaviour
     {
         float horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime);
-    }    
+    }
 }
